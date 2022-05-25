a186372a8b2f87541470adc9cd58182445ed9f14
leandro <leandrosnazareth@gmail.com>
2022-05-25 10:49:43 -0300

    12.9 Corrigindo os testes de integração

---
+++ service/src/main/java/br/com/sw2you/realmeet/config/FilterConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.sw2you.realmeet.config;
+
+import br.com.sw2you.realmeet.domain.repository.ClientRepository;
+import br.com.sw2you.realmeet.filter.VerifyApiKeyFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfiguration {
+    private final ClientRepository clientRepository;
+
+    public FilterConfiguration(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Bean
+    public FilterRegistrationBean<VerifyApiKeyFilter> verifyApiKeyFilter() {
+        var filterFilterRegistrationBean = new FilterRegistrationBean<VerifyApiKeyFilter>();
+        filterFilterRegistrationBean.setFilter(new VerifyApiKeyFilter(clientRepository));
+        filterFilterRegistrationBean.addUrlPatterns("/rooms/*", "/allocations/*");//quais chamadas serão interceptadas
+        return filterFilterRegistrationBean;
+    }
+}
--- service/src/test/java/core/BaseIntegrationTest.java
+++ service/src/test/java/core/BaseIntegrationTest.java
@@ -1,18 +1,27 @@
 package core;
 
+import static org.mockito.BDDMockito.given;
+import static utils.TestConstants.TEST_CLIENT_API_KEY;
+import static utils.TestConstants.TEST_CLIENT_DESCRIPTION;
+
 import br.com.sw2you.realmeet.Application;
 import br.com.sw2you.realmeet.api.ApiClient;
+import br.com.sw2you.realmeet.domain.entity.Client;
+import br.com.sw2you.realmeet.domain.repository.ClientRepository;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 
 @ActiveProfiles(profiles = "integration-test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)//conectar em uma porta random para evitar conflitos
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
+//conectar em uma porta random para evitar conflitos
 public abstract class BaseIntegrationTest {
     @Autowired//iniciar o flyway
     private Flyway flyway;
@@ -20,17 +29,20 @@ public abstract class BaseIntegrationTest {
     @LocalServerPort //setar o valor da porta random
     private int serverPort;
 
+    @MockBean
+    private ClientRepository clientRepository;
+
     @BeforeEach
     void setup() throws Exception {
         setupFlyway();
-//        mockApiKey();
+        mockApiKey();
         setupEach();
     }
 
     protected void setupEach() throws Exception {
     }
 
-    //setar o endereco
+    //setar o endereço
     protected void setLocalHostBasePath(ApiClient apiClient, String path) throws MalformedURLException {
         apiClient.setBasePath(new URL("http", "localhost", serverPort, path).toString());
     }
@@ -40,17 +52,17 @@ public abstract class BaseIntegrationTest {
         flyway.migrate();
     }
 
-//    private void mockApiKey() {
-//        given(clientRepository.findById(TEST_CLIENT_API_KEY))
-//                .willReturn(
-//                        Optional.of(
-//                                Client
-//                                        .newBuilder()
-//                                        .apiKey(TEST_CLIENT_API_KEY)
-//                                        .description(TEST_CLIENT_DESCRIPTION)
-//                                        .active(true)
-//                                        .build()
-//                        )
-//                );
-//    }
+    private void mockApiKey() {
+        given(clientRepository.findById(TEST_CLIENT_API_KEY))
+                .willReturn(
+                        Optional.of(
+                                Client
+                                        .newBuilder()
+                                        .apiKey(TEST_CLIENT_API_KEY)
+                                        .description(TEST_CLIENT_DESCRIPTION)
+                                        .active(true)
+                                        .build()
+                        )
+                );
+    }
 }
